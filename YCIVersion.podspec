37ba451303b40345e2af188fec120c476fd460ed
YanChen-ing <yanchen@ycdeiMac.local>
2019-04-09 11:17:20 +0800

    指定swift 版本 4.0

--- YCIVersion.podspec
+++ YCIVersion.podspec
@@ -17,6 +17,7 @@ Pod::Spec.new do |s|
   s.source           = { :git => 'https://github.com/YanChen-ing/YCIVersion.git', :tag => s.version.to_s }
 
   s.ios.deployment_target = '8.0'
+  s.swift_version = '4.0'
 
   s.source_files = 'YCIVersion/Classes/**/*'
 
--- YCIVersion/Classes/Versionable.swift
+++ YCIVersion/Classes/Versionable.swift
@@ -9,7 +9,7 @@ protocol Versionable {
     
     static var identifier: String {get}
     
-    static var version: VersionNumber {get set}
+    static var currentVersion: VersionNumber {get set}
     
     static func update()
     
@@ -22,22 +22,13 @@ public class YCIVersionManager: NSObject, Versionable {
     
     static var identifier: String = YCIVersionId
     
-    static var version: VersionNumber = 2
+    static var currentVersion: VersionNumber = 2
     
     @objc public static func update() {
-        VersionOperation.start(YCIVersionId, version: version, operation: V1Creation())
+        VersionOperation.start(YCIVersionId, version: currentVersion, operation: V1Creation())
     }
     
 }
-
-/* TODO:
-
- 
- ？减少链的执行次数
- 
- ? 命名优化，OC混编优化
- 
- */
 
 
 
